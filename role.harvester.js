0d6fcc1044646e833c866cbeb5f9eafd6cd6d6af
romanmelnyk <rrmelnyk@gmail.com>
2016-10-11 23:16:57 +0300

    tutorial3

--- role.harvester.js
+++ role.harvester.js
@@ -1,5 +1,6 @@
 var roleHarvester = {
 
+
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
@@ -8,8 +9,16 @@ var roleHarvester = {
             }
         }
         else {
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.energy < structure.energyCapacity;
+                    }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
             }
         }
 	}
