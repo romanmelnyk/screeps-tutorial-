71f1ca1072a8ebb275621aa0c0bb63f450469a8a
romanmelnyk <rrmelnyk@gmail.com>
2016-11-10 17:43:54 +0200

    update of builder

--- role.builder.js
+++ role.builder.js
@@ -2,6 +2,21 @@ var roleBuilder = {
 
 
     run: function(creep) {
+        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+	    var repairWalls = creep.room.find(FIND_STRUCTURES, {
+				filter: (structure) => structure.hits < structure.hitsMax // && structure.structureType == STRUCTURE_WALL
+			});
+		var targetsNeedEnergy = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    }
+            });
+        if((builders.length > 2 && targets.length == 0 && repairWalls.length == 0) || Game.spawns['Spawn1'].energyCapacity > Game.spawns['Spawn1'].energy * 2) {
+            creep.memory.role = 'harvester';
+        }
         if(creep.ticksToLive < 30){
             Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
         }
@@ -18,23 +33,16 @@ var roleBuilder = {
 		//creep builds
 	    if(creep.memory.building) {
 			//creep builds 
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-	        var repairWalls = creep.room.find(FIND_STRUCTURES, {
-				filter: (structure) => structure.hits < structure.hitsMax // && structure.structureType == STRUCTURE_WALL
-			});
             if(targets.length > 0) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
 			//creep repairs Walls
-			else if(repairWalls.length > 0) {
+			else {
 				if(creep.repair(repairWalls[0]) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(repairWalls[0]);
 				}
-			}
-			else {
-			    creep.memory.role = 'upgrader';
 			}
 	    }
 		//creep harvests
@@ -42,8 +50,10 @@ var roleBuilder = {
 	        var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
+                
             }
 	    }
+	    
 	}
 };
 
